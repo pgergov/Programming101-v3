614fc66f15bc4c4983c28f97ed3dae89435fc2a4
Pavlin Gergov <pgergov92@abv.bg>
2015-03-13 03:12:30 +0200

    Added problem 3

---
+++ Last edition warming tasks/Python-file-system-problems/generate_numbers.py
@@ -0,0 +1,15 @@
+import sys
+from random import randint
+
+def main():
+    f = open(sys.argv[1], 'w')
+    for i in range(int(sys.argv[2])):
+        f.write(str(randint(1,1000)) + " ")
+    f.close()
+    
+    f = open(sys.argv[1], 'r')
+    print(f.read())
+    f.close()
+
+if __name__ == '__main__':
+    main()
