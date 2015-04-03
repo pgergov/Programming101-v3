3d385ac662634ac301a611c274a4b03dd093ce40
Pavlin Gergov <pgergov92@abv.bg>
2015-04-03 05:03:10 +0300

    Adding manual tests

--- Last-edition-tasks/4-Dungeons-and-pythons/test.py
+++ Last-edition-tasks/4-Dungeons-and-pythons/test.py
@@ -1,18 +1,45 @@
+with open("basic_dungeon.txt", "r") as f:
+    content = [f.read()]
+print(content)
+
+# [['H.##......'],
+#  ['#.##..###.'],
+#  ['#.###.###.'],
+#  ['#.....###.'],
+#  ['###.#####O']]
 new_content = ""
-with open("basic_dungeon.txt", 'r') as f:
-    content = f.read()
+# if direction = left:
 try:
-    content.index("S")
-    for i in range(len(content) + 1):
-        if i == content.index("S"):
-            print("asd")
-            new_content += "H"
-        else:
-            new_content += content[i]
+    if content[content[0].index("H") + 1] != "#":
+        for i in range(len(content)):
+            if i == content[0].index("H"):
+                content[i] = "."
+            elif i == content[0].index("H") + 1:
+                content[i] = "H"
 except:
     pass
-with open("basic_dungeon.txt", 'w') as f:
-    f.write(new_content)
-
-print(new_content)
 print(content)
+
+# def search(x, y):
+#     if content[x][y] == "O":
+#         print('found at %d,%d' % (x, y))
+#         return True
+#     elif content[x][y] == "#":
+#         print('wall at %d,%d' % (x, y))
+#         return False
+#     # elif content[x][y] == 3:
+#     #     print 'visited at %d,%d' % (x, y)
+#     #     return False
+#     print('visiting %d,%d' % (x, y))
+#     content[x][y] = "#"
+
+#     try:
+#         if ((y > 0 and search(x, y - 1))
+#             or (x > 0 and search(x - 1, y))
+#             or (y < len(content) - 1 and search(x, y+1))
+#             or (x < len(content) - 1 and search(x + 1, y))):
+#             return True
+#     except:
+#         return False
+
+# search(0, 0)
