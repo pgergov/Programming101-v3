18230ddd113dbd81a38ce4aa6561088095766fce
Pavlin Gergov <pgergov92@abv.bg>
2015-04-17 16:00:13 +0300

    Updating tests

--- week5/1-Dungeons-and-Pythons/dungeons_and_pythons_tests.py
+++ week5/1-Dungeons-and-Pythons/dungeons_and_pythons_tests.py
@@ -1,5 +1,6 @@
 from hero import Hero
 from enemy import Enemy
+from dungeon import Dungeon
 from weapons_and_spells import Weapon, Spell
 import unittest
 
@@ -8,6 +9,8 @@ class HeroTests(unittest.TestCase):
     def setUp(self):
         self.hero = Hero("Ahri", "Kingslayer", 100, 100, 2)
         self.enemy = Enemy(100, 100, 20)
+        self.dungeon = Dungeon("level1.txt")
+        self.dungeon.generate_map()
         self.weapon = Weapon("The Axe of Destiny", damage=20)
         self.spell = Spell(
             name="Fireball", damage=30, mana_cost=50, cast_range=2)
@@ -54,6 +57,55 @@ class HeroTests(unittest.TestCase):
         self.assertEqual(self.hero.attack(by="weapon"), 20)
         self.hero.learn(self.spell)
         self.assertEqual(self.hero.attack(by="magic"), 30)
+
+    def test_dungeon_map(self):
+        self.dungeon.generate_map()
+        self.assertEqual(self.dungeon.map, [['S', '.', '#', '#', '.', '.', '.', '.', '.', 'T'], ['#', 'T', '#', '#', '.', '.', '#', '#', '#', '.'], ['#', '.', '#', '#', '#', 'E', '#', '#', '#', 'E'], ['#', '.', 'E', '.', '.', '.', '#', '#', '#', '.'], ['#', '#', '#', 'T', '#', '#', '#', '#', '#', 'G']])
+        # self.dungeon.print_map()
+
+    def test_spawn_hero(self):
+        self.dungeon.spawn(self.hero)
+        self.dungeon.spawn(self.hero)
+        self.dungeon.spawn(self.hero)
+        # self.dungeon.print_map()
+
+    def test_get_position(self):
+        self.dungeon.spawn(self.hero)
+        self.assertEqual(self.dungeon.get_position(), (0, 0))
+
+    def test_move_right(self):
+        self.dungeon.spawn(self.hero)
+        self.dungeon.move_hero("right")
+        # self.dungeon.print_map()
+        self.assertFalse(self.dungeon.move_hero("right"))
+
+    def test_move_left(self):
+        self.dungeon.spawn(self.hero)
+        self.dungeon.move_hero("right")
+        self.assertTrue(self.dungeon.move_hero("left"))
+        # self.dungeon.print_map()
+
+    def test_move_up(self):
+        self.dungeon.spawn(self.hero)
+        self.assertFalse(self.dungeon.move_hero("up"))
+
+    def test_move_down(self):
+        self.dungeon.spawn(self.hero)
+        self.assertFalse(self.dungeon.move_hero("down"))
+
+    def test_moving_around_the_map(self):
+        dungeon = Dungeon("level2.txt")
+        dungeon.generate_map()
+        dungeon.spawn(self.hero)
+        dungeon.move_hero("right")
+        dungeon.move_hero("down")
+        self.assertFalse(dungeon.move_hero("right"))
+        dungeon.move_hero("down")
+        self.assertFalse(dungeon.move_hero("left"))
+        # dungeon.print_map()
+        self.assertTrue(dungeon.move_hero("down"))
+
+
 
 if __name__ == '__main__':
     unittest.main()
